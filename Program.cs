0711be05e808a89040b8778dca062ed0a7c9db58
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-03 20:55:25 -0800

    for loop completed

--- Program.cs
+++ Program.cs
@@ -21,5 +21,11 @@ class Program
             
         } 
         while (attempt <= 3);
+        //for loops
+        for (int i = 1; i <= 4; i++)
+        {
+            Console.WriteLine($"step {i}");
+        }
+        
     }
 }
