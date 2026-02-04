0f8c09fb15bcaa916f3ad201b8212d66a83f65e1
Brandon Silvibarr <brandon.silvi@gmail.com>
2026-02-03 20:19:45 -0800

    WhileLoop section completed

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        //While loops
+        int taskNumber = 1;
+
+        while (taskNumber <= 5)
+        {
+            Console.WriteLine($"Task #{taskNumber} completed");
+            taskNumber++;
+        }
+    }
+}
